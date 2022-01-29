ee1f9699fcceff27bdcb0adadfc9adf2e28b53ef
Scarounet <rondeau.clement@gmail.com>
2022-01-30 00:28:40 +0100

    + play panic animation and set state to Dragged when dragging a character

--- Assets/Scripts/DragAndDrop/Draggable.cs
+++ Assets/Scripts/DragAndDrop/Draggable.cs
@@ -6,11 +6,13 @@ public class Draggable : MonoBehaviour
     private Vector3 m_dragOffset;
     private Camera m_camera;
     private DragManager m_dragManager;
+    private Agent m_agent;
 
     void Awake()
     {
         m_camera = Camera.main;
         m_dragManager = FindObjectOfType<DragManager>();
+        m_agent = GetComponent<Agent>();
     }
 
     void OnMouseDown()
@@ -22,6 +24,8 @@ public class Draggable : MonoBehaviour
     {
         transform.position = GetMousePosition() + m_dragOffset;
         m_dragManager.OnStartDrag(this);
+
+        m_agent.SetState(AgentState.DRAGGED);
     }
 
     private void OnMouseUp()
