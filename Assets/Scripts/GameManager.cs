647b3e2bb659e5da52570626a225472ff7fbd515
Remi minitop <remi@uselessworks.com>
2022-01-29 12:58:45 +0100

    [GameManager] added singleton functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public enum GameState
 {
@@ -11,9 +12,26 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
+    public int seed
+    {
+        get { return m_seed; }
+        set { Assert.IsTrue(m_gameState == GameState.Setup); m_seed = value; }
+    }
+
+    public int localPlayer
+    {
+        get { return m_localPlayer; }
+        set { Assert.IsTrue(m_gameState == GameState.Setup); m_localPlayer = value; }
+    }
+
+    [Header("Internal")]
+    public SetupScreenController setupScreenController; 
+
     // Start is called before the first frame update
     void Start()
     {
+        setupScreenController.gameObject.SetActive(false);
+
         setGameState(GameState.Setup);
     }
 
@@ -33,7 +51,7 @@ public class GameManager : MonoBehaviour
         {
             case GameState.Setup:
             {
-
+                setupScreenController.gameObject.SetActive(false);
             }
             break;
 
@@ -51,7 +69,7 @@ public class GameManager : MonoBehaviour
         {
             case GameState.Setup:
             {
-
+                setupScreenController.gameObject.SetActive(true);
             }
             break;
 
@@ -64,6 +82,19 @@ public class GameManager : MonoBehaviour
     }
 
     GameState m_gameState = GameState.None;
+    
     int m_seed = -1;
     int m_localPlayer = -1;
+
+    // Singleton
+    public static GameManager Get()
+    {
+        if (!m_instance)
+        {
+            m_instance = FindObjectOfType<GameManager>();
+        }
+        Assert.IsNotNull(m_instance);
+        return m_instance;
+    }
+    static GameManager m_instance; 
 }
